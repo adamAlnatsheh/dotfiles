293780bb453923ec25b252aab1a9cf4016de43b7
Adam Al-natsheh <aia@ctrl-labs.com>
2019-05-16 00:25:41 -0400

    take neovim from apt

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!bin/sh
 
 sudo apt update -y
-sudo apt install -y cmake curl gcc g++ htop make ninja-build python3-pip tmux vim wget xclip zsh
+sudo apt install -y cmake curl gcc g++ htop make neovim ninja-build python3-pip tmux vim wget xclip zsh
 
 git clone https://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 
@@ -11,19 +11,15 @@ ln -nfs $HOME/dotfiles/adam1.zsh-theme \
 ln -nfs $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
 ln -fs $HOME/dotfiles/ideavimrc $HOME/.ideavimrc
 
+mkdir -p $HOME/.config/nvim
+ln -nfs $HOME/dotfiles/init.vim $HOME/.config/nvim/init.vim
+
 sudo apt install -y clang-format clang-tools
 
 curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
   > $HOME/Miniconda3-latest-Linux-x86_64.sh
 sh $HOME/Miniconda3-latest-Linux-x86_64.sh
 rm $HOME/Miniconda3-latest-Linux-x86_64.sh
-
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt update -y
-sudo apt install -y neovim
-
-mkdir -p $HOME/.config/nvim
-ln -nfs $HOME/dotfiles/init.vim $HOME/.config/nvim/init.vim
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
